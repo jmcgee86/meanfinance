223f9b32c27efc30daa6bac28f04a3e354690726
Jim McGee <jmcgee86@gmail.com>
2018-06-07 00:05:34 +0000

    find controller ready for buy

--- public/angular-app/find/find-controller.js
+++ public/angular-app/find/find-controller.js
@@ -1,11 +1,20 @@
 angular.module('cdfinance').controller("FindController", FindController);
 
-function FindController($http) {
+function FindController($http, $window, AuthFactory, jwtHelper, $location) {
+
   var vm = this;
+      
+      vm.isLoggedIn = function(){
+        if (AuthFactory.isLoggedIn){
+        return true;
+        }else{
+        return false;
+        }
+      };
+      
   console.log("findController");
   vm.find = function() {
-  var symbol = vm.symbol.toUpperCase()
-
+    var symbol = vm.symbol.toUpperCase();
     console.log(symbol)
     
     $http.get("/api/stocks/" + symbol).then(function(response) {
@@ -17,5 +26,18 @@ function FindController($http) {
         vm.error = error;
       }
     })
+  }
+  
+  vm.buy = function(){
+          var token = $window.sessionStorage.token;
+          var decodedToken = jwtHelper.decodeToken(token);
+          var username = decodedToken.username;
+          var data = {"symbol" : vm.symbol.toUpperCase(), "amount": vm.amount, "price": vm.stockprice}
+
+      $http.post('/api/users/'+ username +"/stocks", data).then(function(response) {
+        //check the responses
+      }).catch(function(error) {
+        console.log(error);
+      })
   }
 }
